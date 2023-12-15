e7872535e5d05c6e94cf7f4768c0867fcadd47c7
Quang Khánh <khanhnguyenmcu@gmail.com>
2023-12-15 13:22:10 +0700

    Update ingredients endpoint

--- src/ingredients/ingredients.module.ts
+++ src/ingredients/ingredients.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { IngredientsService } from './ingredients.service';
 import { IngredientsController } from './ingredients.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Ingredient, IngredientSchema } from './schema/ingredient.schema';
 
 @Module({
   controllers: [IngredientsController],
   providers: [IngredientsService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: Ingredient.name, schema: IngredientSchema },
+    ]),
+  ],
 })
 export class IngredientsModule {}
